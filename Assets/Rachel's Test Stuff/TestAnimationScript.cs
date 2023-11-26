53d107bc1e3f5a7680f0b7ce62fdb7d4d6fa8101
iffycreature01 <150699054+iffycreature01@users.noreply.github.com>
2023-11-26 16:12:01 +0000

    Tweaking animations
    continued working on animations

--- Assets/Rachel's Test Stuff/TestAnimationScript.cs
+++ Assets/Rachel's Test Stuff/TestAnimationScript.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class TestAnimationScript : MonoBehaviour
 {
     int hitList = 0;
-	public Animator arteryDistalAnim;
-    public GameObject blood1;
-    public Material arteryMat;
-    public Material arteryMatDark;
+    public Material transparentMat;
+
+    public Renderer earRen;
+    public Material earMat1, earMat2; //can earMat2 inherit the alpha properties of earMat1?
+    
     public Renderer arteryRen;
+    public Material arteryMat1, arteryMat2;
+    public Animator arteryDistalAnim;
+    public GameObject blood1;
+    //public Renderer blood1Ren;
+    //public Material blood1Mat;
+    public ParticleSystem blood1Part;
+
+    public GameObject blood2;
+    //public Material blood2Mat;
+    public ParticleSystem blood2Part;
 
     public Renderer skullRen;
     public Material skullMat1;
-    public Material transparentMat;
-    //public Color color1;
-    //public Color color2;
+    
 
 	// Start is called before the first frame update
 	void Start()
     {
-        //skullRen.material.color = color1;
+        //set all materials/animations to the right material/state
+        earRen.material = earMat1;
         skullRen.material = skullMat1;
-        arteryRen.material = arteryMat;
+        arteryRen.material = arteryMat1;
         blood1.SetActive(false);
     }
 	
@@ -31,89 +41,68 @@ public class TestAnimationScript : MonoBehaviour
         switch (hitList)
         {
             case 0:
-                StartCoroutine(FadeOut());
-                //ear bruises
+                //first zoom in, adjust material transparency
                 Debug.Log("case 0");
                 break;
             
             case 1:
-                //artery breaks, blood starts flowing
+                //ear bruises
                 Debug.Log("case 1");
-                arteryDistalAnim.SetTrigger("play_artery_break");  //play artery break animation
-                blood1.SetActive(true);  //turn visibility of blood flow on
-                StartCoroutine(ArteryDarken());  //darken distal artery over 5 seconds
+                StartCoroutine(CrossfadeMaterial(1.0f, earRen, earMat1, earMat2));  //fade to bruised ear material
                 break;
             
             case 2:
-                
+                //artery breaks, blood starts flowing
                 Debug.Log("case 2");
+                arteryDistalAnim.SetTrigger("play_artery_break");  //play artery break animation
+                blood1.SetActive(true);  //turn visibility of blood flow on
+                StartCoroutine(CrossfadeMaterial(5.0f, arteryRen, arteryMat1, arteryMat2));  //fade to dark artery material
                 break;
             
             case 3:
+                //hematoma forms
                 Debug.Log("case 3");
+                var slowDown = blood1Part.limitVelocityOverLifetime;
+                slowDown.enabled = true;
+                slowDown.dampen = 0.5f;
+                //StartCoroutine(CrossfadeMaterial(5.0f, blood1Ren, blood1Ren.material, transparentMat));
+                blood2.SetActive(true);
+                //.SetTrigger for skin bulge
                 break;
-
+            
             case 4:
                 Debug.Log("case 4");
                 break;
 
             case 5:
                 Debug.Log("case 5");
+                //StartCoroutine(CrossfadeMaterial(5.0f, arteryRen, arteryMat1, transparentMat));  //fade out distal artery
                 break;
 
             case 6:
                 Debug.Log("case 6");
                 break;
+
+            case 7:
+                Debug.Log("case 7");
+                break;
         }
         hitList += 1;
     }
 
-    private IEnumerator EarBruise()
-    {
-        //can we reuse the same artery darken coroutine? do I have to write another one?
-        //can a coroutine take in a parameter?
-        yield return null;
-    }
-    
-    private IEnumerator ArteryDarken()
-        //fades one artery material into another
+    private IEnumerator CrossfadeMaterial(float duration, Renderer ren, Material mat1, Material mat2)
+    //fades one material into another over a defined duration of time
     {
         float elapsedTime = 0f;
-        float duration = 5.0f;
 
         while (elapsedTime < duration)
         {
             elapsedTime += Time.deltaTime;
-            arteryRen.material.Lerp(arteryMat, arteryMatDark, elapsedTime / duration);
+            ren.material.Lerp(mat1, mat2, elapsedTime / duration);
             yield return null;
         }
     }
 
-    private IEnumerator FadeOut()
-    {
-        float elapsedTime = 0f;
-        float duration = 1.0f;
-
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            skullRen.material.Lerp(skullMat1, transparentMat, elapsedTime / duration);
-            yield return null;
-        }
-    }
-
-    //private IEnumerator FadeOut()
-    //{
-    //    float elapsedTime = 0f;
-    //    float duration = 1.0f;
-
-    //    while (elapsedTime < duration)
-    //    {
-    //        elapsedTime += Time.deltaTime;
-    //        skullRen.material.color = Color.Lerp(color1, color2, elapsedTime / duration);
-    //        yield return null;
-    //    }   
-    //}
     //https://stackoverflow.com/questions/64510141/using-a-coroutine-in-unity3d-to-fade-a-game-object-out-and-back-in-looping-inf
 
 }
